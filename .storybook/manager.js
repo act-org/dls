99be0f616cd76cb0695fc8d3cf2c813ea1b83e7e
Jon Cursi <joncursi@gmail.com>
2020-06-18 00:56:28 -0400

    feat: Add custom branding

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,6 +3,7 @@
  */
 
 import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
 
 addons.setConfig({
   enableShortcuts: true,
@@ -12,4 +13,9 @@ addons.setConfig({
   showNav: true,
   showPanel: true,
   sidebarAnimations: true,
+  theme: {
+    ...themes.light,
+    // brandImage: 'https://www.act.org/etc/designs/act/clientlibs/act-base/img/logo-act-blue-300.png',
+    brandTitle: 'ACT Design Language System',
+  },
 });
