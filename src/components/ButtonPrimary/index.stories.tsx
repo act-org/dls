e9ed1759e4fd45046cd52cb986ab7f21a77e6b65
Jon Cursi <joncursi@gmail.com>
2020-06-18 00:41:20 -0400

    feat: Story improvement

--- src/components/ButtonPrimary/index.stories.tsx
+++ src/components/ButtonPrimary/index.stories.tsx
@@ -7,25 +7,27 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
+import { Plus as PlusIcon } from 'mdi-material-ui';
 
 import ButtonPrimary from '.';
 
 export const example = (): React.ReactElement<any> => (
   <ButtonPrimary
-    disabled={boolean('disabled', false)}
-    fullWidth={boolean('fullWidth', false)}
+    disabled={boolean('Disabled?', false)}
+    fullWidth={boolean('Full Width?', false)}
     onClick={action('onClick()')}
     size={select(
-      'size',
+      'Size',
       {
-        small: 'small',
-        medium: 'medium',
-        large: 'large',
+        Small: 'small',
+        Medium: 'medium',
+        Large: 'large',
       },
       'medium',
     )}
+    startIcon={boolean('Start Icon?', true) ? <PlusIcon /> : undefined}
   >
-    {text('children', 'Click Me')}
+    {text('Text', 'Click Me')}
   </ButtonPrimary>
 );
 
