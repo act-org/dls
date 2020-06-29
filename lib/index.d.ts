ceae3c118f6fa4ad1efcb896e128c36a93486e9b
Jon Cursi <joncursi@gmail.com>
2020-06-29 00:24:50 -0400

    fix: Remove unused index file

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @prettier
- */
-import * as components from './components';
-import * as constants from './constants';
-import * as types from './types';
-export { components, constants, types };
-//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @prettier
- */
-
-import * as components from './components';
-import * as constants from './constants';
-import * as types from './types';
-
-export { components, constants, types };
