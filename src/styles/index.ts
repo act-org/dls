323108938d63d24679d918f2382eb06e74ab85e9
Vijay Ambati <vijay.ambati@act.org>
2020-07-13 12:07:50 -0500

    fix: Terraform

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,5 +4,5 @@
 
 export { default as createTheme } from './createTheme';
 export { default as makeStyles } from './makeStyles';
-export { default as ServerStyleSheets } from './ServerStyleSheets';
+export { default as ServerStyleSheets } from './serverStyleSheets';
 export { default as theme } from './theme';
