051bf799b456f8a3c3fc167840cd4ff75cc241d9
Vijay Ambati <vijay.ambati@act.org>
2020-07-13 12:08:12 -0500

    fix: Terraform

---
+++ tf/README.md
@@ -0,0 +1,8 @@
+## deploy sattic content
+
+\$ ./deploy-static.sh
+
+## Terraform scripts
+
+terraform init
+terraform apply -var-file="dev.tfvars" 
---
+++ tf/outputs.tf
@@ -0,0 +1,10 @@
+output "cdn_domain_name" {
+  description = "Domain name of the Cloudfront Distribution"
+  value       = "${aws_cloudfront_distribution.cdn.domain_name}"
+}
+
+output "domain" {
+  description = "Final Domain "
+  value       = "https://${var.domain_name}"
+}
+
