53846a891b267a05209dbe7505c542d496ba980b
Jon Cursi <joncursi@gmail.com>
2020-07-15 18:22:01 -0400

    fix: Re-adds ServerStyleSheets

--- lib/styles/index.d.ts
+++ lib/styles/index.d.ts
@@ -3,5 +3,6 @@
  */
 export { default as createTheme } from './createTheme';
 export { default as makeStyles } from './makeStyles';
+export { default as ServerStyleSheets } from './ServerStyleSheets';
 export { default as theme } from './theme';
 //# sourceMappingURL=index.d.ts.map
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,4 +4,5 @@
 
 export { default as createTheme } from './createTheme';
 export { default as makeStyles } from './makeStyles';
+export { default as ServerStyleSheets } from './ServerStyleSheets';
 export { default as theme } from './theme';
