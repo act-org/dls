97a34b950b08247767d6e5b745b37ae33181a410
Jon Cursi <joncursi@gmail.com>
2023-04-13 17:44:13 -0400

    feat: Added spacing visualizer

---
+++ src/components/_foundations/Spacing/index.stories.tsx
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) ACT, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @prettier
+ */
+
+import { Grid, TextField, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+
+import { StyledGridItem, StyledPaper } from './styles';
+
+const Story = (): React.ReactElement => {
+  const { spacing } = useTheme();
+  const [sp, setSp] = React.useState(1);
+
+  return (
+    <>
+      <Grid container>
+        <Grid item>
+          <TextField
+            InputProps={{
+              inputProps: {
+                min: 0,
+              },
+            }}
+            label="Spacing"
+            onChange={(e): void => {
+              setSp(Number(e.target.value || 0));
+            }}
+            type="number"
+            value={sp}
+            variant="outlined"
+          />
+        </Grid>
+
+        <StyledGridItem item>
+          <Typography>= {spacing(sp)}</Typography>
+        </StyledGridItem>
+      </Grid>
+
+      <StyledPaper
+        sx={{
+          p: spacing(sp),
+        }}
+        variant="outlined"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </StyledPaper>
+    </>
+  );
+};
+
+export default {
+  args: {},
+  component: Story,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+  title: 'Foundations / Spacing',
+} as Meta;
+
+type Story = StoryObj;
+
+export const Preview: Story = { args: {} };
