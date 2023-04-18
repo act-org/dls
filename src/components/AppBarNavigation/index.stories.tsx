06db3a5832c033c0322fed33503da856faa877dd
Jon Cursi <joncursi@gmail.com>
2023-04-18 10:30:12 -0400

    refactor: Convert AppBarNavigation component stories to CSF3

---
+++ src/components/AppBarNavigation/index.stories.tsx
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) ACT, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @prettier
+ */
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import { AppBarNavigation, AppBarNavigationProps } from '.';
+
+/**
+ * This component uses App Bar to create a top-level navigation header.
+ */
+export default {
+  args: {
+    AppBarProps: { position: 'static' },
+    navigationItems: [
+      { isActive: true, LinkProps: { to: '#' }, title: 'Home' },
+      { isActive: false, LinkProps: { to: '#' }, title: 'Other Page' },
+    ],
+    RenderLogoProps: {
+      alt: 'ACT',
+      LinkProps: { to: 'https://act-et.org' },
+      src: 'https://i.imgur.com/P0gtzO0.png',
+    },
+  },
+  argTypes: {},
+  component: AppBarNavigation,
+  tags: ['autodocs'],
+  title: 'Organisms / AppBarNavigation',
+} as Meta<AppBarNavigationProps>;
+
+export const WithOneLink: StoryObj<AppBarNavigationProps> = {
+  args: {
+    navigationItems: [
+      { isActive: true, LinkProps: { to: '#' }, title: 'Home' },
+    ],
+  },
+};
+
+export const WithManyLinks: StoryObj<AppBarNavigationProps> = {
+  args: {
+    navigationItems: [
+      { isActive: true, LinkProps: { to: '#' }, title: 'Home' },
+      { isActive: false, LinkProps: { to: '#' }, title: 'Other Page 1' },
+      { isActive: false, LinkProps: { to: '#' }, title: 'Other Page 2' },
+      { isActive: false, LinkProps: { to: '#' }, title: 'Other Page 3' },
+    ],
+  },
+};
