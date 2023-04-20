94d4361f6d31b162dbe4c2a62a99059f5b52b1ed
johnny.li <johny.li@act-et.org>
2023-04-20 14:34:37 -0700

    docs: Added stories for Rating

--- src/components/_muiCore/Rating/index.stories.tsx
+++ src/components/_muiCore/Rating/index.stories.tsx
@@ -8,8 +8,10 @@
  */
 
 import { Meta, StoryObj } from '@storybook/react';
+import { ReactElement, useState } from 'react';
 
-import { Rating } from './internal';
+import { Rating, RatingProps } from './internal';
+import Box from '@mui/material/Box';
 
 /**
  * Renders a standard Material UI Button using the
@@ -27,3 +29,57 @@ export default {
 } as Meta<Rating>;
 
 export const Primary: StoryObj<Rating> = { args: {} };
+
+export const DefaultValue: StoryObj<Rating> = { args: { defaultValue: 4 } };
+
+export const Disabled: StoryObj<Rating> = {
+  args: { defaultValue: 3, disabled: true },
+};
+
+export const EmptyLabelText: StoryObj<Rating> = {
+  args: { emptyLabelText: 'Select a number of stars' },
+};
+
+const labels: { [index: string]: string } = {
+  1: 'Very Negative',
+  2: 'Negative',
+  3: 'Slightly Negative',
+  4: 'Slightly Positive',
+  5: 'Positive',
+  6: 'Very Positive',
+};
+function getLabelText(value: number) {
+  return `${labels[value]}`;
+}
+
+export const HoverLabel = {
+  render: (args: RatingProps): ReactElement<unknown> => {
+    const [value, setValue] = useState<number | null>(2);
+    const [hover, setHover] = useState(-1);
+    return (
+      <Box sx={{ width: '300px' }}>
+        <Rating
+          {...args}
+          getLabelText={getLabelText}
+          max={6}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            setHover(newHover);
+          }}
+          value={value}
+        />
+        {value !== null && (
+          <Box component="span" sx={{ ml: 3 }}>
+            {labels[hover !== -1 ? hover : value]}
+          </Box>
+        )}
+      </Box>
+    );
+  },
+};
+
+export const MaxMin: StoryObj<Rating> = { args: { max: 17 } };
+
+export const Precision: StoryObj<Rating> = { args: { precision: 0.1 } };
