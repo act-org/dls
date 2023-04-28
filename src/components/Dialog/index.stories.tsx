ac616012253a561e71aa856c8b61018e94e2f8fd
johnny.li <johny.li@act-et.org>
2023-04-28 08:18:19 -0700

    docs: Refactor Dialog stories

--- src/components/Dialog/index.stories.tsx
+++ src/components/Dialog/index.stories.tsx
@@ -8,7 +8,6 @@
  */
 
 import {
-  Box,
   Button,
   DialogActions,
   DialogContent,
@@ -16,7 +15,7 @@ import {
   PaperProps,
 } from '@mui/material';
 import { Meta, StoryFn, StoryObj } from '@storybook/react';
-import { ReactElement, useState } from 'react';
+import { useState } from 'react';
 import Draggable from 'react-draggable';
 
 import { Playground } from '@actinc/dls/helpers/playground';
@@ -116,144 +115,110 @@ export const FullWidth: StoryObj<DialogProps> = {
 };
 
 export const ScrollBody = {
-  render: (args: DialogProps): ReactElement<unknown> => {
-    const [open, setOpen] = useState(false);
-    return (
-      <>
-        <Box>
-          <Button
-            onClick={(): void => {
-              setOpen(true);
-            }}
-          >
-            open
-          </Button>
-        </Box>
-        <Dialog {...args} open={open} scroll="body" title="Lorem ipsum">
-          <DialogContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            et ex efficitur, fringilla magna a, accumsan mi. Nunc tincidunt
-            pretium dolor non fermentum. Sed non facilisis erat. Sed dictum erat
-            ut justo convallis, ut gravida massa congue. Donec suscipit lacus
-            nec enim fringilla congue. Nulla lectus justo, viverra nec mattis
-            in, congue at lacus. Donec aliquet aliquam enim nec finibus. Cras
-            condimentum aliquam vulputate. Nam in tempor purus. In condimentum
-            tincidunt mi eu sollicitudin. Phasellus eu urna eu odio condimentum
-            interdum. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Fusce mollis lacus a
-            sagittis porttitor. Fusce ut libero blandit, facilisis eros sit
-            amet, posuere massa. Nunc sed nibh accumsan, lobortis ante quis,
-            pellentesque libero. Curabitur et ipsum lorem. Fusce condimentum
-            consequat arcu, at vulputate enim mollis sed. Etiam vel diam
-            gravida, lobortis tortor nec, elementum est. Integer cursus nisi
-            leo, sit amet tempus libero porta quis. Ut id faucibus diam,
-            ultricies ultricies tortor. In ut neque at ipsum placerat auctor sed
-            nec turpis. Morbi gravida, neque in imperdiet facilisis, tortor quam
-            tincidunt felis, sit amet aliquet ante urna sollicitudin justo.
-            Aliquam fringilla in sapien quis sagittis. Donec viverra quam non
-            laoreet suscipit. Integer vel consectetur sem, vel ornare erat.
-            Phasellus blandit scelerisque velit, at mollis purus dictum non.
-            Nullam malesuada sit amet arcu eu faucibus. Nam non nibh felis. Nam
-            quis mattis nisi. In semper mi eu mauris dignissim, sed suscipit
-            ligula semper. Aliquam malesuada velit felis, sit amet vulputate
-            dolor efficitur vitae. Curabitur fringilla purus eget elit posuere,
-            et porttitor libero convallis. Donec condimentum justo eu odio
-            rhoncus, sit amet venenatis sapien porttitor. Vestibulum ornare
-            vitae felis sed consectetur. Pellentesque nec cursus risus. Mauris
-            tempor ut ante venenatis imperdiet. Vestibulum id posuere purus.
-            Suspendisse nec eleifend quam. Donec id rhoncus ipsum, sed aliquet
-            erat. Maecenas est felis, eleifend eu hendrerit sit amet, lacinia id
-            turpis. Donec cursus magna eu purus cursus, eget accumsan ipsum
-            porttitor. Maecenas ullamcorper nulla id magna sollicitudin euismod.
-            Integer cursus pellentesque arcu et finibus. Pellentesque vel est
-            posuere, dignissim ligula sit amet, euismod justo. Aliquam id
-            volutpat erat. Sed tempor feugiat semper. Vivamus quam leo,
-            venenatis eget quam in, ornare pretium eros. Sed blandit eros ac sem
-            sagittis elementum. Fusce vel nibh pellentesque, viverra metus et,
-            efficitur tortor. Sed lectus ligula, aliquet sed hendrerit nec,
-            maximus non metus. Orci varius natoque penatibus et magnis dis
-            parturient montes, nascetur ridiculus mus. Sed varius ex vitae lacus
-            finibus elementum. Aliquam lectus arcu, gravida in tristique eget,
-            ultricies maximus nibh. Curabitur iaculis turpis at leo volutpat, id
-            bibendum tellus ultricies. Curabitur vel tellus vel mauris rhoncus
-            elementum quis vestibulum ex. Sed porta dapibus tincidunt.
-            Vestibulum vitae purus mauris. Pellentesque congue enim eget cursus
-            laoreet.
-          </DialogContent>
-        </Dialog>
-      </>
-    );
+  args: {
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+et ex efficitur, fringilla magna a, accumsan mi. Nunc tincidunt
+pretium dolor non fermentum. Sed non facilisis erat. Sed dictum erat
+ut justo convallis, ut gravida massa congue. Donec suscipit lacus
+nec enim fringilla congue. Nulla lectus justo, viverra nec mattis
+in, congue at lacus. Donec aliquet aliquam enim nec finibus. Cras
+condimentum aliquam vulputate. Nam in tempor purus. In condimentum
+tincidunt mi eu sollicitudin. Phasellus eu urna eu odio condimentum
+interdum. Class aptent taciti sociosqu ad litora torquent per
+conubia nostra, per inceptos himenaeos. Fusce mollis lacus a
+sagittis porttitor. Fusce ut libero blandit, facilisis eros sit
+amet, posuere massa. Nunc sed nibh accumsan, lobortis ante quis,
+pellentesque libero. Curabitur et ipsum lorem. Fusce condimentum
+consequat arcu, at vulputate enim mollis sed. Etiam vel diam
+gravida, lobortis tortor nec, elementum est. Integer cursus nisi
+leo, sit amet tempus libero porta quis. Ut id faucibus diam,
+ultricies ultricies tortor. In ut neque at ipsum placerat auctor sed
+nec turpis. Morbi gravida, neque in imperdiet facilisis, tortor quam
+tincidunt felis, sit amet aliquet ante urna sollicitudin justo.
+Aliquam fringilla in sapien quis sagittis. Donec viverra quam non
+laoreet suscipit. Integer vel consectetur sem, vel ornare erat.
+Phasellus blandit scelerisque velit, at mollis purus dictum non.
+Nullam malesuada sit amet arcu eu faucibus. Nam non nibh felis. Nam
+quis mattis nisi. In semper mi eu mauris dignissim, sed suscipit
+ligula semper. Aliquam malesuada velit felis, sit amet vulputate
+dolor efficitur vitae. Curabitur fringilla purus eget elit posuere,
+et porttitor libero convallis. Donec condimentum justo eu odio
+rhoncus, sit amet venenatis sapien porttitor. Vestibulum ornare
+vitae felis sed consectetur. Pellentesque nec cursus risus. Mauris
+tempor ut ante venenatis imperdiet. Vestibulum id posuere purus.
+Suspendisse nec eleifend quam. Donec id rhoncus ipsum, sed aliquet
+erat. Maecenas est felis, eleifend eu hendrerit sit amet, lacinia id
+turpis. Donec cursus magna eu purus cursus, eget accumsan ipsum
+porttitor. Maecenas ullamcorper nulla id magna sollicitudin euismod.
+Integer cursus pellentesque arcu et finibus. Pellentesque vel est
+posuere, dignissim ligula sit amet, euismod justo. Aliquam id
+volutpat erat. Sed tempor feugiat semper. Vivamus quam leo,
+venenatis eget quam in, ornare pretium eros. Sed blandit eros ac sem
+sagittis elementum. Fusce vel nibh pellentesque, viverra metus et,
+efficitur tortor. Sed lectus ligula, aliquet sed hendrerit nec,
+maximus non metus. Orci varius natoque penatibus et magnis dis
+parturient montes, nascetur ridiculus mus. Sed varius ex vitae lacus
+finibus elementum. Aliquam lectus arcu, gravida in tristique eget,
+ultricies maximus nibh. Curabitur iaculis turpis at leo volutpat, id
+bibendum tellus ultricies. Curabitur vel tellus vel mauris rhoncus
+elementum quis vestibulum ex. Sed porta dapibus tincidunt.
+Vestibulum vitae purus mauris. Pellentesque congue enim eget cursus
+laoreet.`,
+    scroll: 'body',
   },
 };
 
 export const ScrollPaper = {
-  render: (args: DialogProps): ReactElement<unknown> => {
-    const [open, setOpen] = useState(false);
-    return (
-      <>
-        <Box>
-          <Button
-            onClick={(): void => {
-              setOpen(true);
-            }}
-          >
-            open
-          </Button>
-        </Box>
-        <Dialog {...args} open={open} scroll="paper" title="Lorem ipsum">
-          <DialogContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            et ex efficitur, fringilla magna a, accumsan mi. Nunc tincidunt
-            pretium dolor non fermentum. Sed non facilisis erat. Sed dictum erat
-            ut justo convallis, ut gravida massa congue. Donec suscipit lacus
-            nec enim fringilla congue. Nulla lectus justo, viverra nec mattis
-            in, congue at lacus. Donec aliquet aliquam enim nec finibus. Cras
-            condimentum aliquam vulputate. Nam in tempor purus. In condimentum
-            tincidunt mi eu sollicitudin. Phasellus eu urna eu odio condimentum
-            interdum. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Fusce mollis lacus a
-            sagittis porttitor. Fusce ut libero blandit, facilisis eros sit
-            amet, posuere massa. Nunc sed nibh accumsan, lobortis ante quis,
-            pellentesque libero. Curabitur et ipsum lorem. Fusce condimentum
-            consequat arcu, at vulputate enim mollis sed. Etiam vel diam
-            gravida, lobortis tortor nec, elementum est. Integer cursus nisi
-            leo, sit amet tempus libero porta quis. Ut id faucibus diam,
-            ultricies ultricies tortor. In ut neque at ipsum placerat auctor sed
-            nec turpis. Morbi gravida, neque in imperdiet facilisis, tortor quam
-            tincidunt felis, sit amet aliquet ante urna sollicitudin justo.
-            Aliquam fringilla in sapien quis sagittis. Donec viverra quam non
-            laoreet suscipit. Integer vel consectetur sem, vel ornare erat.
-            Phasellus blandit scelerisque velit, at mollis purus dictum non.
-            Nullam malesuada sit amet arcu eu faucibus. Nam non nibh felis. Nam
-            quis mattis nisi. In semper mi eu mauris dignissim, sed suscipit
-            ligula semper. Aliquam malesuada velit felis, sit amet vulputate
-            dolor efficitur vitae. Curabitur fringilla purus eget elit posuere,
-            et porttitor libero convallis. Donec condimentum justo eu odio
-            rhoncus, sit amet venenatis sapien porttitor. Vestibulum ornare
-            vitae felis sed consectetur. Pellentesque nec cursus risus. Mauris
-            tempor ut ante venenatis imperdiet. Vestibulum id posuere purus.
-            Suspendisse nec eleifend quam. Donec id rhoncus ipsum, sed aliquet
-            erat. Maecenas est felis, eleifend eu hendrerit sit amet, lacinia id
-            turpis. Donec cursus magna eu purus cursus, eget accumsan ipsum
-            porttitor. Maecenas ullamcorper nulla id magna sollicitudin euismod.
-            Integer cursus pellentesque arcu et finibus. Pellentesque vel est
-            posuere, dignissim ligula sit amet, euismod justo. Aliquam id
-            volutpat erat. Sed tempor feugiat semper. Vivamus quam leo,
-            venenatis eget quam in, ornare pretium eros. Sed blandit eros ac sem
-            sagittis elementum. Fusce vel nibh pellentesque, viverra metus et,
-            efficitur tortor. Sed lectus ligula, aliquet sed hendrerit nec,
-            maximus non metus. Orci varius natoque penatibus et magnis dis
-            parturient montes, nascetur ridiculus mus. Sed varius ex vitae lacus
-            finibus elementum. Aliquam lectus arcu, gravida in tristique eget,
-            ultricies maximus nibh. Curabitur iaculis turpis at leo volutpat, id
-            bibendum tellus ultricies. Curabitur vel tellus vel mauris rhoncus
-            elementum quis vestibulum ex. Sed porta dapibus tincidunt.
-            Vestibulum vitae purus mauris. Pellentesque congue enim eget cursus
-            laoreet.
-          </DialogContent>
-        </Dialog>
-      </>
-    );
+  args: {
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+et ex efficitur, fringilla magna a, accumsan mi. Nunc tincidunt
+pretium dolor non fermentum. Sed non facilisis erat. Sed dictum erat
+ut justo convallis, ut gravida massa congue. Donec suscipit lacus
+nec enim fringilla congue. Nulla lectus justo, viverra nec mattis
+in, congue at lacus. Donec aliquet aliquam enim nec finibus. Cras
+condimentum aliquam vulputate. Nam in tempor purus. In condimentum
+tincidunt mi eu sollicitudin. Phasellus eu urna eu odio condimentum
+interdum. Class aptent taciti sociosqu ad litora torquent per
+conubia nostra, per inceptos himenaeos. Fusce mollis lacus a
+sagittis porttitor. Fusce ut libero blandit, facilisis eros sit
+amet, posuere massa. Nunc sed nibh accumsan, lobortis ante quis,
+pellentesque libero. Curabitur et ipsum lorem. Fusce condimentum
+consequat arcu, at vulputate enim mollis sed. Etiam vel diam
+gravida, lobortis tortor nec, elementum est. Integer cursus nisi
+leo, sit amet tempus libero porta quis. Ut id faucibus diam,
+ultricies ultricies tortor. In ut neque at ipsum placerat auctor sed
+nec turpis. Morbi gravida, neque in imperdiet facilisis, tortor quam
+tincidunt felis, sit amet aliquet ante urna sollicitudin justo.
+Aliquam fringilla in sapien quis sagittis. Donec viverra quam non
+laoreet suscipit. Integer vel consectetur sem, vel ornare erat.
+Phasellus blandit scelerisque velit, at mollis purus dictum non.
+Nullam malesuada sit amet arcu eu faucibus. Nam non nibh felis. Nam
+quis mattis nisi. In semper mi eu mauris dignissim, sed suscipit
+ligula semper. Aliquam malesuada velit felis, sit amet vulputate
+dolor efficitur vitae. Curabitur fringilla purus eget elit posuere,
+et porttitor libero convallis. Donec condimentum justo eu odio
+rhoncus, sit amet venenatis sapien porttitor. Vestibulum ornare
+vitae felis sed consectetur. Pellentesque nec cursus risus. Mauris
+tempor ut ante venenatis imperdiet. Vestibulum id posuere purus.
+Suspendisse nec eleifend quam. Donec id rhoncus ipsum, sed aliquet
+erat. Maecenas est felis, eleifend eu hendrerit sit amet, lacinia id
+turpis. Donec cursus magna eu purus cursus, eget accumsan ipsum
+porttitor. Maecenas ullamcorper nulla id magna sollicitudin euismod.
+Integer cursus pellentesque arcu et finibus. Pellentesque vel est
+posuere, dignissim ligula sit amet, euismod justo. Aliquam id
+volutpat erat. Sed tempor feugiat semper. Vivamus quam leo,
+venenatis eget quam in, ornare pretium eros. Sed blandit eros ac sem
+sagittis elementum. Fusce vel nibh pellentesque, viverra metus et,
+efficitur tortor. Sed lectus ligula, aliquet sed hendrerit nec,
+maximus non metus. Orci varius natoque penatibus et magnis dis
+parturient montes, nascetur ridiculus mus. Sed varius ex vitae lacus
+finibus elementum. Aliquam lectus arcu, gravida in tristique eget,
+ultricies maximus nibh. Curabitur iaculis turpis at leo volutpat, id
+bibendum tellus ultricies. Curabitur vel tellus vel mauris rhoncus
+elementum quis vestibulum ex. Sed porta dapibus tincidunt.
+Vestibulum vitae purus mauris. Pellentesque congue enim eget cursus
+laoreet.`,
+    scroll: 'paper',
   },
 };
 
