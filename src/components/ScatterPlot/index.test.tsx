37683897d8f4b522183f6e23ab9769b79ca825e5
Jon Cursi <joncursi@gmail.com>
2024-03-07 04:39:56 -0400

    test: Added tests

---
+++ src/components/ScatterPlot/index.test.tsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) ACT, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @prettier
+ */
+
+import { mockResizeObserver, standard } from '~/helpers/test';
+
+import { ScatterPlot } from '.';
+
+const DATA = [
+  { label: 'FAFSA', x: 0.9, y: 32 },
+  { label: 'SAT Scores', x: 0.85, y: 27 },
+  { label: 'Website Inquiry', x: 1.8, y: 23 },
+];
+
+mockResizeObserver();
+
+describe('ScatterPlot', () => {
+  const Component = (
+    <ScatterPlot
+      data={DATA}
+      xLabelValue="Inquiries to Applicants"
+      yLabelValue="Inquiries"
+    />
+  );
+  standard(Component);
+});
