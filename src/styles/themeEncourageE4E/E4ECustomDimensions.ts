70a835555833fba3314b473344db1b63ac7d3b87
Patrick Matos <patrickmatos@ENCSABCAMLT0557>
2024-08-26 11:22:48 -0300

    feat: Add Encourage E4E theme

---
+++ src/styles/themeEncourageE4E/E4ECustomDimensions.ts
@@ -0,0 +1,19 @@
+const E4ECustomDimensions = {
+  heights: {
+    dialogContainer: 32,
+    dialogMargin: 44,
+    header: 64,
+    tableHeader: 48,
+    tablePagination: 58,
+  },
+  widths: {
+    datePickerPopper: 277,
+    largeDialog: 872,
+    leftNavBar: 72,
+    listItemIcon: 40,
+    mediumDialog: 648,
+    pageMainContent: 1320,
+  },
+};
+
+export default E4ECustomDimensions;
---
+++ src/styles/themeEncourageE4E/props.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line regex/invalid-error
+import { ComponentsProps } from '@mui/material';
+
+const props: ComponentsProps = {
+  MuiButton: {
+    disableRipple: true,
+  },
+  MuiButtonBase: {
+    disableRipple: true,
+  },
+};
+
+export default props;
