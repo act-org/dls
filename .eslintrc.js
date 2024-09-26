7ee0e2b86fa21694d7987d599bd4af2b44392439
Jon Cursi <joncursi@gmail.com>
2024-09-26 17:03:24 -0400

    build: More eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,65 +6,8 @@
  */
 
 module.exports = {
-  extends: [
-    '@actinc/eslint-config',
-    'plugin:mdx/recommended',
-    'plugin:storybook/recommended',
-  ],
-  overrides: [
-    {
-      files: ['./**/*.stories.template.tsx', './**/*.stories.tsx'],
-      rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        'storybook/story-exports': 'off',
-      },
-    },
-  ],
+  extends: ['@actinc/eslint-config'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          caseInsensitive: true,
-          order: 'asc',
-        },
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
-        ],
-        'newlines-between': 'always',
-        pathGroups: [
-          {
-            group: 'internal',
-            pattern: '~/**',
-          },
-          {
-            group: 'internal',
-            pattern: '~/icons/**',
-          },
-          {
-            group: 'external',
-            pattern: '@**',
-          },
-        ],
-        warnOnUnassignedImports: true,
-      },
-    ],
-    'react/boolean-prop-naming': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/require-default-props': 'off',
-  },
-  settings: {
-    'mdx/code-blocks': true,
-    // optional, if you want to disable language mapper, set it to `false`
-    // if you want to override the default language mapper inside, you can provide your own
-    'mdx/language-mapper': {},
   },
 };
