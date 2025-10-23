403242842ed009ae21a90592a5482512076e49ec
Jon Cursi <joncursi@gmail.com>
2025-10-23 17:17:07 -0400

    docs: Updated Autocomplete stories

--- src/components/_muiCore/Autocomplete/index.stories.tsx
+++ src/components/_muiCore/Autocomplete/index.stories.tsx
@@ -5,12 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { AutocompleteRenderInputParams, Chip, TextField } from '@mui/material';
+import { AutocompleteRenderInputParams, TextField } from '@mui/material';
 import { Meta, StoryObj } from '@storybook/react-webpack5';
-import { ReactElement } from 'react';
+
+import ThemeProvider from '~/components/ThemeProvider';
+import { createThemeStory } from '~/helpers/createThemeStory';
+import { Playground } from '~/helpers/playground';
+import { ThemesArray } from '~/styles/themes';
 
 import { Autocomplete, AutocompleteProps, FilmType, topFilms } from './internal';
 
+/**
+ * The default Autocomplete exports
+ */
 export default {
   args: {
     disablePortal: false,
@@ -25,56 +32,79 @@ export default {
   title: 'MUI Core / Autocomplete',
 } as Meta<Autocomplete>;
 
-export const Primary: StoryObj<Autocomplete> = { args: { color: 'primary' } };
+type Story = StoryObj<AutocompleteProps<FilmType, false, false, false>>;
 
-export const Multiple = {
-  render: (args: AutocompleteProps<FilmType, true, false, false, typeof Chip>): ReactElement<unknown> => (
-    <Autocomplete
-      {...args}
-      getOptionLabel={(option): string => option.label}
-      multiple
-      renderTags={(value: FilmType[], getTagProps): ReactElement<unknown>[] =>
-        value.map((option: FilmType, index: number) => (
-          // eslint-disable-next-line react/jsx-key
-          <Chip label={option.label} variant="outlined" {...getTagProps({ index })} />
-        ))
-      }
-    />
-  ),
+// Documentation story (not snapshotted in Chromatic)
+export const Documentation: Story = {
+  args: {
+    options: topFilms,
+    renderInput: (params: AutocompleteRenderInputParams) => <TextField {...params} label="Movie" />,
+  },
+  parameters: {
+    chromatic: { disable: true },
+  },
 };
 
-export const WithAutocomplete: StoryObj<Autocomplete> = {
-  args: { autoComplete: true },
+// Playground story (not snapshotted in Chromatic)
+export const PlaygroundStory: Story = {
+  args: {
+    options: topFilms,
+    renderInput: (params: AutocompleteRenderInputParams) => <TextField {...params} label="Movie" />,
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  argTypes: Playground({}, Autocomplete),
+  name: 'Playground',
+  parameters: {
+    chromatic: { disable: true },
+  },
 };
 
-export const WithoutAutocomplete: StoryObj<Autocomplete> = {
-  args: { autoComplete: false },
-};
+// Theme-specific stories (snapshotted in Chromatic)
+// Generate stories for each theme dynamically
 
-export const WithAutohighlight: StoryObj<Autocomplete> = {
-  args: { autoHighlight: true },
-};
+// Export theme-specific stories dynamically
+const themeStories = ThemesArray.reduce(
+  (stories, theme) => {
+    // eslint-disable-next-line no-param-reassign
+    stories[theme] = createThemeStory<AutocompleteProps<FilmType, false, false, false>>(theme, {
+      render: () => (
+        <ThemeProvider theme={theme}>
+          {/* Default Autocomplete */}
+          <Autocomplete options={topFilms} renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} label="Default" />} />
 
-export const WithAutoselect: StoryObj<Autocomplete> = {
-  args: { autoSelect: true },
-};
+          {/* Multiple Selection */}
+          <Autocomplete
+            multiple
+            options={topFilms}
+            renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} label="Multiple" />}
+            style={{ marginTop: 16 }}
+          />
 
-export const WithAutohighlightAndAutoSelect: StoryObj<Autocomplete> = {
-  args: { autoHighlight: true, autoSelect: true },
-};
+          {/* Free Solo */}
+          <Autocomplete
+            freeSolo
+            options={topFilms}
+            renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} label="Free Solo" />}
+            style={{ marginTop: 16 }}
+          />
 
-export const FreeSolo: StoryObj<Autocomplete> = {
-  args: { freeSolo: true },
-};
+          {/* With Auto Highlight */}
+          <Autocomplete
+            autoHighlight
+            options={topFilms}
+            renderInput={(params: AutocompleteRenderInputParams) => <TextField {...params} label="Auto Highlight" />}
+            style={{ marginTop: 16 }}
+          />
+        </ThemeProvider>
+      ),
+    });
 
-export const IncludeInputInList: StoryObj<Autocomplete> = {
-  args: { includeInputInList: true },
-};
+    return stories;
+  },
+  {} as Record<string, Story>,
+);
 
-export const OpenOnFocus: StoryObj<Autocomplete> = {
-  args: { openOnFocus: true },
-};
-
-export const SelectOnFocus: StoryObj<Autocomplete> = {
-  args: { selectOnFocus: true },
-};
+export const ThemeEncoura = { ...themeStories.ENCOURA, name: 'Theme: Encoura' };
+export const ThemeEncouraClassic = { ...themeStories.ENCOURA_CLASSIC, name: 'Theme: Encoura Classic' };
+export const ThemeEncourage = { ...themeStories.ENCOURAGE, name: 'Theme: Encourage' };
+export const ThemeEncourageE4E = { ...themeStories.ENCOURAGE_E4E, name: 'Theme: Encourage E4E' };
