91f881c3d9ea1dc72de77fb7356d2bb4c36ad570
Jon Cursi <joncursi@gmail.com>
2025-10-23 18:55:51 -0400

    docs: Added Dialog stories

---
+++ src/components/_muiCore/Dialog/internal.tsx
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Encoura, LLC and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Dialog as MuiDialog, DialogProps as MuiDialogProps } from '@mui/material';
+
+export const Dialog = MuiDialog;
+export type Dialog = typeof MuiDialog;
+export type DialogProps = MuiDialogProps;
